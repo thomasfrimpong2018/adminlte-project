3a71f061e18494ef5b953c4e7f92c0b57b9226d7
Thomas Frimpong <tfrimpong1987@gmail.com>
2018-06-23 14:49:34 +0100

    default page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,6 +1,22 @@
 @extends('layouts.master')
 @section('content')
- 
+<div class="row">
+    <div class="col-md-10">
+      <div class="box">
+        <div class="box-header with-border">
+          <h3 class="box-title">Monthly Recap Report</h3>
+
+         
+        </div>
+        <!-- /.box-header -->
+        <div class="box-body">
+          
+        </div>
+        <!-- ./box-body -->
+        <div class="box-footer">
+         
+          </div>
+          <!-- /.row -->
 
 
 @endsection
