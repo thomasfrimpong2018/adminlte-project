aecf53c8b56cc320480f29e994d2b570254455e2
Thomas Frimpong <tfrimpong1987@gmail.com>
2018-06-24 14:27:08 +0100

    delete modal added

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -138,7 +138,35 @@
           </div>
           <!-- /.modal -->
 
+   <!---Delete Modal-->
 
+   <div class="modal modal-danger fade" id="modal-delete"  tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span></button>
+            <h4 class="modal-title">Delete Category</h4>
+          </div>
+        <form action="{{ route('category.destroy','category') }}" method="POST">
+              {{ csrf_field() }}
+              {{method_field('delete')}}
+          <div class="modal-body">
+            <input name="id"  type="hidden" id="id">
+            Are you sure you want to delete this category?
+
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-default pull-left" data-dismiss="modal">Close</button>
+            <button type="submit" class="btn btn-primary">Delete Category</button>
+          </div>
+      </form>
+        </div>
+        <!-- /.modal-content -->
+      </div>
+      <!-- /.modal-dialog -->
+    </div>
+    <!-- /.modal -->
 
 
 @endsection
